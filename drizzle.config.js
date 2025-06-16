16ce5deae8abbf6317ed48c7de132069f4cbb155
jaiyankargupta <jaiyankargupta@gmail.com>
2025-06-16 18:41:11 +0530

    fixes

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
   schema: ["./configs/schema.js"],
   dialect: "postgresql",
   dbCredentials: {
-    url: "postgresql://ai-video_owner:npg_GBqOJmFv9UK8@ep-fragrant-wave-a8kk1810-pooler.eastus2.azure.neon.tech/ai-video?sslmode=require",
+    url: `${process.env.NEXT_PUBLIC_DATABASE_URL}`,
   },
 };
